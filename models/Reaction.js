e4c5a4a0efa1aa48e3b43a926059cd1b3fc524fd
Fancy Pants <edaily94@gmail.com>
2023-05-08 18:53:17 -0700

    completed reaction.js

--- models/Reaction.js
+++ models/Reaction.js
@@ -5,5 +5,32 @@ const reactionSchema = new Schema(
         reactionId: {
             type: Schema.Types.ObjectId,
             default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280,
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (date) => date.toLocaleDateString(),
         }
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        _id: false,
     })
+
+    module.exports = reactionSchema
