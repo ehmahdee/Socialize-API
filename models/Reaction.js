9a1c2e47c209b89e8a1db4d13ad8929ca570af8c
Fancy Pants <edaily94@gmail.com>
2023-05-08 18:45:03 -0700

    started reaction.js

---
+++ models/Reaction.js
@@ -0,0 +1,9 @@
+const { Schema, Types } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        }
+    })
